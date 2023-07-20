ea6dd15cc24518e68251966a89b9f8746a83c4bd
Mick <tambov.solid@bk.ru>
2023-07-21 00:55:16 +0600

    Task06 pass

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int div = number % 2;
+
+if(div == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
