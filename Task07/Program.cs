f72a148c346e6b3157669cf72e7358c5676d293a
Mick <tambov.solid@bk.ru>
2023-07-21 00:29:50 +0600

    Task07

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//получение последней цифры числа
+
+Console.WriteLine("Введите трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number *= -1; // преобразование в положительное если отрицательное
+}
+
+if (number >= 100 && number <= 999) // проверка трехначности
+{
+    int lastDigit = number % 10; //остаток деления на 10  456%10 = 6
+    Console.Write("Последняя цифра числа :" + lastDigit);
+}
+    else
+    {
+        Console.WriteLine("Ошибка ввода");
+    }
