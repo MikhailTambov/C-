3533f4081e10bd865bf3c39d445855db2165a648
Mick <tambov.solid@bk.ru>
2023-07-21 00:50:44 +0600

    Task04 pass

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int max = 0;
+
+Console.WriteLine("Введите первое число:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число:");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > max)
+{
+    max = firstNumber;
+}
+if(secondNumber > max)
+{
+    max = secondNumber;
+}
+if(thirdNumber > max)
+{
+    max = thirdNumber;
+}
+
+Console.WriteLine($"max = {max}");
