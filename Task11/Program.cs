b9175f0236837f99fe39c40006490f58601d8445
Mick <tambov.solid@bk.ru>
2023-07-21 22:48:23 +0600

    Task13

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next (100, 1000);
+Console. WriteLine($"Случайное трехзначное число -> {number}");
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+int resultDigit = firstDigit * 10 + thirdDigit;
+Console.WriteLine($"Ответ: {resultDigit}");
+
+int result = RemoveSeconDigit(number);
+Console.WriteLine($"Ответ: {result}");
+
+int RemoveSeconDigit (int num)
+{
+int firstDigit = num / 100;
+int thirdDigit = num % 10;
+int resultDigit = firstDigit * 10 + thirdDigit;
+return resultDigit;
+}
