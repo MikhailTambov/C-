b7e2e74ad3e95fb933523876f249ab0d43759a6f
Mick <tambov.solid@bk.ru>
2023-07-21 22:27:05 +0600

    Task09 pass and add Task11

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,7 +1,22 @@
 ﻿int number = new Random().Next(10, 100);
 Console.WriteLine ($"Случайное число от 10 до 99 -> {number}");
-int firstDigit = number / 10; //отбрасывается дробная часть от реззультата деления
-int secondDigit = number % 10; //остаток деления на 10
-if (firstDigit > secondDigit) Console.WriteLine($"Максимальная цифра числа {firstDigit}");
-else Console.WriteLine($"Максимальная цифра числа {secondDigit}");
+// int firstDigit = number / 10; //отбрасывается дробная часть от реззультата деления
+// int secondDigit = number % 10; //остаток деления на 10
+// if (firstDigit > secondDigit) Console.WriteLine($"Максимальная цифра числа {firstDigit}");
+// else Console.WriteLine($"Максимальная цифра числа {secondDigit}");
 
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Максимальная цифра числа - {maxDigit}");
+int maxDigit1 = MaxDigit(56);
+Console.WriteLine($"Максимальная цифра числа - {maxDigit1}");
+int maxDigit2 = MaxDigit(89);
+Console.WriteLine($"Максимальная цифра числа - {maxDigit2}");
+
+
+int MaxDigit (int num)  
+{
+    int firstDigit = num / 10; //отбрасывается дробная часть от реззультата деления
+    int secondDigit = num % 10; //остаток деления на 10
+    if (firstDigit > secondDigit) return firstDigit;
+    return secondDigit;
+}
