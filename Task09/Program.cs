c3d14db15b89ef2273e5d96a0fff89bd8c0e1cf8
Mick <tambov.solid@bk.ru>
2023-07-21 22:13:17 +0600

    Task09 pass

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine ($"Случайное число от 10 до 99 -> {number}");
+int firstDigit = number / 10; //отбрасывается дробная часть от реззультата деления
+int secondDigit = number % 10; //остаток деления на 10
+if (firstDigit > secondDigit) Console.WriteLine($"Максимальная цифра числа {firstDigit}");
+else Console.WriteLine($"Максимальная цифра числа {secondDigit}");
+
