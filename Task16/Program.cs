686febb27bbce4d45fb57f5175dc51f65191e6b6
Mick <tambov.solid@bk.ru>
2023-07-23 18:48:15 +0600

    Task 16

---
+++ Task16/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int div = number % 2;
+
+if(div == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
